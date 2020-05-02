193a3bca523bccfc4217dac5062704a83e7cbee6
Suresh Nagulapalli <sureshnagulapalli13@gmail.com>
2020-05-02 00:58:18 -0400

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent,
+    EducationComponent,
+    ProjectsComponent,
+    AboutComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
